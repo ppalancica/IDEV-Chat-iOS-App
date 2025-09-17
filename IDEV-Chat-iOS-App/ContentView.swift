e6acb83b67b4eb0ae982f98d682ea672b44ec0d7
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-17 18:47:35 +0300

    Added some basic UI components

--- IDEV-Chat-iOS-App/ContentView.swift
+++ IDEV-Chat-iOS-App/ContentView.swift
@@ -2,9 +2,48 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Login Page")
-            .padding()
+        NavigationView {
+            ScrollView {
+                Picker(selection: $isLoginMode,
+                       label: Text("Picker here")) {
+                    Text("Login").tag(true)
+                    Text("Create Account").tag(false)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.vertical, 12)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 64))
+                        .padding()
+                }
+                
+                TextField("Email", text: $email)
+                TextField("Password", text: $password)
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 12)
+                        Spacer()
+                    }
+                    .background(.blue)
+                }
+            }
+            .navigationTitle("Create Account")
+        }
+        .padding()
     }
 }
 
