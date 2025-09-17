9cbccedef94d3d057b1bb4ffdeedbf9f5fdda621
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-17 18:48:45 +0300

    Embedded UI elements in a VStack inside ScrollView

--- IDEV-Chat-iOS-App/ContentView.swift
+++ IDEV-Chat-iOS-App/ContentView.swift
@@ -9,41 +9,43 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                Picker(selection: $isLoginMode,
-                       label: Text("Picker here")) {
-                    Text("Login").tag(true)
-                    Text("Create Account").tag(false)
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.vertical, 12)
-                
-                Button {
-                    
-                } label: {
-                    Image(systemName: "person.fill")
-                        .font(.system(size: 64))
-                        .padding()
-                }
-                
-                TextField("Email", text: $email)
-                TextField("Password", text: $password)
-                
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Spacer()
-                        Text("Create Account")
-                            .foregroundColor(.white)
-                            .padding(.vertical, 12)
-                        Spacer()
+                VStack {
+                    Picker(selection: $isLoginMode,
+                           label: Text("Picker here")) {
+                        Text("Login").tag(true)
+                        Text("Create Account").tag(false)
                     }
-                    .background(.blue)
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.vertical, 12)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    
+                    TextField("Email", text: $email)
+                    TextField("Password", text: $password)
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 12)
+                            Spacer()
+                        }
+                        .background(.blue)
+                    }
                 }
+                .padding()
             }
             .navigationTitle("Create Account")
         }
-        .padding()
     }
 }
 
